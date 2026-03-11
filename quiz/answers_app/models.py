3bfb88fcfdc1314afed6768b4981d3c487116335
Harpreetkaur05 <harpreetkaur26505@gmail.com>
2026-03-11 21:16:27 +0530

    First project upload

---
+++ quiz/answers_app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from quizapp.models import Quiz
+from django.contrib.auth.models import User
+from questions_app.models import questions
+class Answers(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    quiz = models.ForeignKey(Quiz,on_delete=models.CASCADE)
+    question = models.ForeignKey(questions,on_delete=models.CASCADE)
+    score = models.IntegerField(default=0)
+    selected_option=models.IntegerField(null=True)
